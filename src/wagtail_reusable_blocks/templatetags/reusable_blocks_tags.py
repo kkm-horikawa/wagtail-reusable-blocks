3281f0dba1e3c6e57f980367c20aa9ddb4b58621
Makoto Horikawa <makoto10.devcre21@kkm-mako.com>
2025-12-14 04:03:00 +0900

    test: Add GIF rendering tests for ImageBlock
    - Add get_test_gif_file helper to create test GIF images
    - Add TestImageBlockGifRendering test class
    - Add type annotation to is_gif filter to fix mypy error
    
    Github-Issue: #192

--- src/wagtail_reusable_blocks/templatetags/reusable_blocks_tags.py
+++ src/wagtail_reusable_blocks/templatetags/reusable_blocks_tags.py
@@ -1,6 +1,7 @@
 """Template tags and filters for wagtail-reusable-blocks."""
 
 import os
+from typing import Any
 
 from django import template
 
@@ -8,7 +9,7 @@ register = template.Library()
 
 
 @register.filter
-def is_gif(image):
+def is_gif(image: Any) -> bool:
     """Check if an image is a GIF file.
 
     Args:
@@ -20,4 +21,4 @@ def is_gif(image):
     if not image or not hasattr(image, "file") or not image.file:
         return False
     _, ext = os.path.splitext(image.file.name)
-    return ext.lower() == ".gif"
+    return bool(ext.lower() == ".gif")
